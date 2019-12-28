95d42a0603339322989f7b76ad69b5b9d5941ad9
James Downs <jamesdownslab@gmail.com>
2019-12-28 18:41:46 +0000

    works for part 2

--- Day15/day15code.py
+++ Day15/day15code.py
@@ -2,6 +2,7 @@ import intcode
 import random
 import matplotlib.pyplot as plt
 from queue import Queue
+from collections import defaultdict
 
 
 class Wall:
@@ -96,6 +97,42 @@ class RepairDroid:
         approaches = {1: "S", 2: "N", 3: "E", 4: "W"}
         return approaches[command]
 
+    def generate_graph(self, oxygen):
+        graph = {}
+        tile = self.corridors[oxygen]
+        tiles_to_graph = Queue()
+        tiles_to_graph.put(tile)
+        while not tiles_to_graph.empty():
+            tile = tiles_to_graph.get()
+            adjacent_tiles = self.adjacent_tiles(tile)
+            if (tile.x, tile.y) not in graph.keys():
+                graph[(tile.x, tile.y)] = [(t.x, t.y) for t in adjacent_tiles]
+                for t in adjacent_tiles:
+                    tiles_to_graph.put(t)
+        self.graph = Graph(graph)
+        return self.graph
+
+    def adjacent_tiles(self, tile):
+        x, y = tile.x, tile.y
+        tiles = []
+        possible_keys = [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+        for key in possible_keys:
+            if key in self.corridors.keys():
+                tiles.append(self.corridors[key])
+        return tiles
+
+    def fill_oxygen(self, oxygen):
+        # find the distance between all tiles and oxygen
+        furthest = 0
+        tile = self.corridors[oxygen]
+        other_tiles = [c for key, c in self.corridors.items() if key != oxygen]
+        for t in other_tiles:
+            paths = self.graph.find_all_paths((tile.x, tile.y), (t.x, t.y))
+            length = min([len(p) for p in paths])
+            if length > furthest:
+                furthest = length
+        return furthest
+
 
 def plot_tiles(tiles):
     wall_x = []
@@ -116,6 +153,86 @@ def plot_map(walls, oxygen):
     plt.show()
 
 
+class Graph(object):
+
+    def __init__(self, graph_dict=None):
+        """ initializes a graph object
+            If no dictionary or None is given,
+            an empty dictionary will be used
+        """
+        if graph_dict == None:
+            graph_dict = {}
+        self.__graph_dict = graph_dict
+
+    def vertices(self):
+        """ returns the vertices of a graph """
+        return list(self.__graph_dict.keys())
+
+    def edges(self):
+        """ returns the edges of a graph """
+        return self.__generate_edges()
+
+    def add_vertex(self, vertex):
+        """ If the vertex "vertex" is not in
+            self.__graph_dict, a key "vertex" with an empty
+            list as a value is added to the dictionary.
+            Otherwise nothing has to be done.
+        """
+        if vertex not in self.__graph_dict:
+            self.__graph_dict[vertex] = []
+
+    def add_edge(self, edge):
+        """ assumes that edge is of type set, tuple or list;
+            between two vertices can be multiple edges!
+        """
+        edge = set(edge)
+        (vertex1, vertex2) = tuple(edge)
+        if vertex1 in self.__graph_dict:
+            self.__graph_dict[vertex1].append(vertex2)
+        else:
+            self.__graph_dict[vertex1] = [vertex2]
+
+    def __generate_edges(self):
+        """ A static method generating the edges of the
+            graph "graph". Edges are represented as sets
+            with one (a loop back to the vertex) or two
+            vertices
+        """
+        edges = []
+        for vertex in self.__graph_dict:
+            for neighbour in self.__graph_dict[vertex]:
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+        return edges
+
+    def __str__(self):
+        res = "vertices: "
+        for k in self.__graph_dict:
+            res += str(k) + " "
+        res += "\nedges: "
+        for edge in self.__generate_edges():
+            res += str(edge) + " "
+        return res
+
+    def find_all_paths(self, start_vertex, end_vertex, path=[]):
+        """ find all paths from start_vertex to
+            end_vertex in graph """
+        graph = self.__graph_dict
+        path = path + [start_vertex]
+        if start_vertex == end_vertex:
+            return [path]
+        if start_vertex not in graph:
+            return []
+        paths = []
+        for vertex in graph[start_vertex]:
+            if vertex not in path:
+                extended_paths = self.find_all_paths(vertex,
+                                                     end_vertex,
+                                                     path)
+                for p in extended_paths:
+                    paths.append(p)
+        return paths
+
 
 
 if __name__ == "__main__":
@@ -126,3 +243,7 @@ if __name__ == "__main__":
     print(oxygen)
     print(distance)
     plot_tiles(repair_droid.walls)
+    print(repair_droid.generate_graph(oxygen))
+    # print(len(repair_droid.shortest_path(oxygen, (0, 0))))
+    minutes = repair_droid.fill_oxygen(oxygen) - 1
+    print("mintutes to fill oxygen : ", minutes)
